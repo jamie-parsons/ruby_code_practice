06ae26b711fcfd0d614a7ede6d5acdf531366d39
jamie-parsons <jhelenparsons@gmail.com>
2021-05-15 19:56:05 -0600

    exercises

--- ruby_code_practice/symbols.rb
+++ ruby_code_practice/symbols.rb
@@ -1,36 +1,63 @@
 
+def pig_it(text)
+  newT = text.chars
+  
+  puts newT.map.include?(" ") ? newT.select : newT[4]
+ end
 
+nine = 'Pig latin is cool'
+
+pig_it(nine)
+
+
+# def sum_two_smallest_numbers(numbers)
+#         #sums = [] << numbers.sort
+#         #total = sums.inject{|a| a(0..1)}
+#         total = numbers.min(3).sum
+#         puts total
+
+#         #sums.map{|a| a.sum = total }
+        
+#       puts total
+# end
+
+
+# doit = [4, 2, 1, 5, 9]
+
+# sum_two_smallest_numbers(doit)
+
+#puts doit
 
 # def unique_in_order(iterable)
 #     newArr = (iterable.to_s.split('').chunk {|a| a} .map(&:first))
-#     puts newArr
+# #     puts newArr
+# # end
+# def unique_in_order(iterable)
+#     if iterable == []
+#       return []
+#     elsif iterable.is_a?(Integer)
+#       puts "Inte"
+#     end
+#     newArr = (iterable.to_s.split('').chunk {|a| a} .map(&:first))
+#     #if newArr == []
+      
 # end
-def unique_in_order(iterable)
-    if iterable == []
-      return []
-    elsif iterable.is_a?(Integer)
-      puts "Inte"
-    end
-    newArr = (iterable.to_s.split('').chunk {|a| a} .map(&:first))
-    #if newArr == []
+
+# st = 123442335
+
+# unique_in_order(st)
+
+# def unique_in_order(iterable)
+#     if iterable == []
+#       return []
+#     elsif iterable[5].is_a?(Integer)
+#       newArr = (iterable{|a| a}.map(&:first)).chunk
+#     else
+#     newArr = (iterable.to_s.split('').chunk {|a| a} .map(&:first))
+#     #if newArr == []
+#       end
       
-end
-
-st = 123442335
-
-unique_in_order(st)
-
-def unique_in_order(iterable)
-    if iterable == []
-      return []
-    elsif iterable[5].is_a?(Integer)
-      newArr = (iterable{|a| a}.map(&:first)).chunk
-    else
-    newArr = (iterable.to_s.split('').chunk {|a| a} .map(&:first))
-    #if newArr == []
-      end
-      
-end
+# end
 
 # def is_ruby_pass_by_value?(foo)
 #     foo << <<~NOTE
